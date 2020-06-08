b17ac70824b2c293dc74a66a37fff8b41f099207
yamam00s <s.yamamo0327@gmail.com>
2020-06-08 23:41:30 +0900

    breakTimeごとにブラウザに通知

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { Meetings } from 'types/response'
 import CreateMeeting from 'containers/CreateMeeting'
+import CountDown from 'containers/CountDown'
 
 const Home: FC = () => {
   const [meetings, setMeetings] = useState<Meetings>(null)
+  const [isBreak, setIsBreak] = useState(false)
+
+  const countDown = {
+    // 3時間
+    totalTime: 10800,
+    // 37分（3分前）
+    breakTime: 2280,
+    setIsBreak
+  }
+
+  useEffect(() => {
+    Notification.requestPermission()
+  }, [])
+
+  useEffect(() => {
+    const isNotification = Notification.permission === 'granted'
+    if (isNotification && isBreak) {
+      new Notification('残り3分です。新規ミーティングを作成しましょう。')
+    }
+    setIsBreak(false)
+  }, [isBreak])
+
   return (
     <>
+      <CountDown countDownProps={countDown} />
       <CreateMeeting setResponse={setMeetings} />
       {meetings && (<p>{meetings.join_url}</p>)}
     </>
